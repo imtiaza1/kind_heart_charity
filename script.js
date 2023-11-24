af70c4aa43db94c3bc7b2122da86dadd77c5fc6c
imtiaz12344 <emtiazaleeee@gmail.com>
2023-11-24 05:03:02 +0500

    file is updated

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 // slider
+// const sliders = document.querySelectorAll(".slider");
+// const leftBtn = document.querySelector(".left-btn");
+// const rightBtn = document.querySelector(".right-btn");
+
+// let currentSlideIndex = 0; // Keeps track of the current slide index
+
+// // Function to display the current slide and hide others
+// function showSlide(index) {
+// 	sliders.forEach((slider, i) => {
+// 		if (i === index) {
+// 			slider.style.opacity = "1";
+// 			slider.style.display = "block";
+// 			slider.style.transition = "opacity 0.5s";
+// 		} else {
+// 			slider.style.opacity = "0";
+// 			slider.style.display = "none";
+// 			slider.style.transition = "none";
+// 		}
+// 	});
+// }
+
+// // Initial display
+// showSlide(currentSlideIndex);
+
 const sliders = document.querySelectorAll(".slider");
 const leftBtn = document.querySelector(".left-btn");
 const rightBtn = document.querySelector(".right-btn");
@@ -11,17 +35,26 @@ function showSlide(index) {
 		if (i === index) {
 			slider.style.opacity = "1";
 			slider.style.display = "block";
-			slider.style.transition = "opacity 0.5s";
+			slider.style.transition = "opacity 1.5s all";
 		} else {
 			slider.style.opacity = "0";
 			slider.style.display = "none";
-			slider.style.transition = "none";
+			slider.style.transition = "opacity 1.5s all";
 		}
 	});
 }
 
+// Function to show the next slide
+function showNextSlide() {
+	currentSlideIndex = (currentSlideIndex + 1) % sliders.length;
+	showSlide(currentSlideIndex);
+}
+
 // Initial display
 showSlide(currentSlideIndex);
+
+// Automatically change slide every 1 second
+setInterval(showNextSlide, 2000);
 
 // Event listener for the left button
 leftBtn.addEventListener("click", () => {
